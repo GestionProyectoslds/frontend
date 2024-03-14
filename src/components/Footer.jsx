d39b3467132e51773370fe03b8ad0d73163e3a25
ningy <saulfavela01@gmail.com>
2024-03-14 14:33:41 -0600

    Se modificó el footer y se aplicaron correcciones en la taskboardpage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,24 +3,28 @@ import Logo from "../images/footer/Logo.png";
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-4 px-4 items-center gap-4 w-full">
-      <div className="flex items-center gap-4 w-full">
-        <img
-          src={Logo}
-          alt="Community Lab Alliance Logo"
-          className="w-12 h-12 rounded-full ml-10"
-        />
-        <a className="font-bold text-gray-200 text-[15px] ml-10 hover:text-gray-100">
-          Community Lab Alliance
-        </a>
-        <a href="https://drive.google.com/file/d/11_DZ7m2lQnkoFVbLEGhag4W_XRqcM-on/view" className="font-bold text-lime-500 text-[15px] ml-72 hover:text-lime-500">
-          Aviso de Privacidad
-        </a>
-        <a className="font-bold text-gray-200 text-[15px] ml-80 hover:text-gray-100">
-          @2023 Community Lab Alliance
-        </a>
+    <footer className="bg-gray-800 text-white py-4 px-4 w-full">
+      <div className="flex flex-col md:flex-row items-center justify-between">
+        <div className="flex items-center gap-4">
+          <img
+            src={Logo}
+            alt="Community Lab Alliance Logo"
+            className="w-12 h-12 rounded-full"
+          />
+          <a className="font-bold text-gray-200 text-sm hover:text-gray-100">
+            Community Lab Alliance
+          </a>
+        </div>
+        <div className="flex flex-col md:flex-row items-center">
+          <a href="https://drive.google.com/file/d/11_DZ7m2lQnkoFVbLEGhag4W_XRqcM-on/view" className="font-bold text-lime-500 text-sm mx-4 my-2 md:my-0 hover:text-lime-500 block text-center md:inline">
+            Aviso de Privacidad
+          </a></div>
+          <span className="font-bold text-gray-200 text-sm">
+            @ 2023 Community Lab Alliance
+          </span>
       </div>
     </footer>
   );
 }
+
 export default Footer;
--- src/pages/TaskBoardPage.jsx
+++ src/pages/TaskBoardPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from '../components/Header';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faLink, faEllipsisV } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faLink, faEllipsisV, faClock, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import CreateList from '../components/homePage-components/AddTaskBoard';
 
@@ -85,7 +85,7 @@ const TaskBoardPage = () => {
             <button
               type="button"
               className="ml-5 bg-white border-gray-300 rounded-lg"
-              
+
             >
               <FontAwesomeIcon
                 icon={faPlus}
@@ -105,34 +105,111 @@ const TaskBoardPage = () => {
           </div>
         </div>
 
+
+
+
         {/* Nueva sección con rectángulos verticales */}
         <div className="flex mt-4">
           {/* Rectángulo Pending */}
-          <div className="flex-1 bg-red-500 p-4 rounded-md">
-            <div className="flex justify-between items-center mb-4">
-              <span className="text-white text-lg font-bold">Pending</span>
-              <FontAwesomeIcon icon={faEllipsisV} className="text-white" />
+          <section className="flex-1">
+            <div className="flex-1 bg-[#EF5350] h-10 flex items-center justify-between">
+              <span className="text-white text-base font-bold ml-5">Pending</span>
+              <FontAwesomeIcon icon={faEllipsisV} className="text-white mr-5" />
             </div>
-            {renderActivities(pendingActivities)}
-          </div>
+            {/* ----------------- Ésta parte sería el componente de cada actividad -------------- */}
+            {/*Parte 1 */}
+            <div className="bg-[#FEF7F6] w-full pt-4">
+              <div className="bg-white border border-[#E3E3E3] mx-4 mb-4">
+                <div className="my-2 mx-4">
+                  <div className=" flex justify-between">
+                    <label className="text-sm font-bold">Website Redesign</label>
+                    <FontAwesomeIcon icon={faChevronDown} />
+                  </div>
+                  <div className="flex items-center my-2"> {/* Contenedor del texto y la barra de progreso */}
+                    <div class="w-full bg-gray-300 rounded-full h-1.5"> {/* Barra de progreso */}
+                      <div class="bg-[#EF5350] h-1.5 rounded-full" style={{ width: '50%' }}></div>
+                    </div>
+                  </div>
+                  <div className="text-sm">
+                    <FontAwesomeIcon icon={faClock} />
+                    <label className="ml-1">Sep 26</label>
+                  </div>
+                  <div className="text-sm flex justify-between">
+                    <div className="bg-[#FDE2E7] rounded-full">
+                      <label className=" text-[#EF5350] text-xs font-bold mx-2">High</label>
+                    </div>
+                    <div>
+                      <label>Usuarios</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <label className=" font-semibold mb-4">Add New Task</label>
+              </div>
+            </div>
+          </section>
 
           {/* Rectángulo In Progress */}
-          <div className="flex-1 bg-blue-500 p-4 rounded-lg ml-4">
-            <div className="flex justify-between items-center mb-4">
-              <span className="text-white text-lg font-bold">In Progress</span>
-              <FontAwesomeIcon icon={faEllipsisV} className="text-white" />
+          <section className="flex-1 rounded-lg ml-4">
+            <div className="flex-1 bg-[#42A5F5] h-10 flex items-center justify-between">
+              <span className="text-white text-base font-bold ml-5">Completed</span>
+              <FontAwesomeIcon icon={faEllipsisV} className="text-white mr-5" />
             </div>
-            {renderActivities(inProgressActivities)}
-          </div>
+            {/* ----------------- Ésta parte sería el componente de cada actividad -------------- */}
+            <div className="bg-[#E7F3FE] w-full h-full pt-3">
+              <div className="bg-white border border-[#E3E3E3] mt-3 mx-4 -mb-3">
+                <div className="my-2 mx-4">
+                  <label className="text-sm font-bold">Website Redesign</label>
+                  <div className="flex items-center my-2"> {/* Contenedor del texto y la barra de progreso */}
+                    <div class="w-full bg-gray-300 rounded-full h-1.5"> {/* Barra de progreso */}
+                      <div class="bg-[#42A5F5] h-1.5 rounded-full" style={{ width: '50%' }}></div>
+                    </div>
+                  </div>
+                  <div className="text-sm">
+                    <FontAwesomeIcon icon={faClock} />
+                    <label className="ml-1">Sep 26</label>
+                  </div>
+                  <div className="text-sm flex justify-between">
+                    <label>Prioridad</label>
+                    <label>Usuarios</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {renderActivities(pendingActivities)}
+          </section>
+
 
           {/* Rectángulo Completed */}
-          <div className="flex-1 bg-green-500 p-4 rounded-lg ml-4">
-            <div className="flex justify-between items-center mb-4">
-              <span className="text-white text-lg font-bold">Completed</span>
-              <FontAwesomeIcon icon={faEllipsisV} className="text-white" />
+          <section className="flex-1 rounded-lg ml-4">
+            <div className="flex-1 bg-[#4CAF50] h-10 flex items-center justify-between">
+              <span className="text-white text-base font-bold ml-5">Completed</span>
+              <FontAwesomeIcon icon={faEllipsisV} className="text-white mr-5" />
             </div>
-            {renderActivities(completedActivities)}
-          </div>
+            {/* ----------------- Ésta parte sería el componente de cada actividad -------------- */}
+            <div className="bg-[#EDF7EE] w-full h-full pt-3">
+              <div className="bg-white border border-[#E3E3E3] mt-3 mx-4 -mb-3">
+                <div className="my-2 mx-4">
+                  <label className="text-sm font-bold">Website Redesign</label>
+                  <div className="flex items-center my-2"> {/* Contenedor del texto y la barra de progreso */}
+                    <div class="w-full bg-gray-300 rounded-full h-1.5"> {/* Barra de progreso */}
+                      <div class="bg-[#4CAF50] h-1.5 rounded-full" style={{ width: '50%' }}></div>
+                    </div>
+                  </div>
+                  <div className="text-sm">
+                    <FontAwesomeIcon icon={faClock} />
+                    <label className="ml-1">Sep 26</label>
+                  </div>
+                  <div className="text-sm flex justify-between">
+                    <label>Prioridad</label>
+                    <label>Usuarios</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {renderActivities(pendingActivities)}
+          </section>
         </div>
 
         {/* Modal para crear lista */}
